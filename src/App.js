c37228f6232e076531164e84b66658aea5808032
nickmwangemi <nickmwangemi@gmail.com>
2021-01-08 16:14:11 +0300

    Setup final app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,69 @@
 import { useState } from 'react'
 import './App.css'
+import Todo from './components/Todo'
 
 function App() {
 	const [todos, setTodos] = useState([
-		{ text: 'Learn about React' },
-		{ text: 'Meet friend for lunch' },
-		{ text: 'Build really cool todo app' },
+		{
+			text: 'Learn about React',
+			isCompleted: false,
+		},
+		{
+			text: 'Meet friend for lunch',
+			isCompleted: false,
+		},
+		{
+			text: 'Build really cool todo app',
+			isCompleted: false,
+		},
 	])
 
-	return <div></div>
+	const addTodo = (text) => {
+		const newTodos = [...todos, { text }]
+		setTodos(newTodos)
+	}
+
+	function TodoForm() {
+		const [value, setValue] = useState('')
+
+		const handleSubmit = (e) => {
+			e.preventDefault()
+			if (!value) return
+			addTodo(value)
+			setValue('')
+		}
+		return (
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					className='input'
+					value={value}
+					onChange={(e) => setValue(e.target.value)}
+				/>
+			</form>
+		)
+	}
+
+	const completeTodo = (index) => {
+		const newTodos = [...todos]
+		newTodos[index].isCompleted = true
+		setTodos(newTodos)
+	}
+	return (
+		<div className='app'>
+			<div className='todo-list'>
+				{todos.map((todo, index) => (
+					<Todo
+						key={index}
+						index={index}
+						todo={todo}
+						completeTodo={completeTodo}
+					/>
+				))}
+				<TodoForm addTodo={addTodo} />
+			</div>
+		</div>
+	)
 }
 
 export default App
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,15 @@
-function Todo({ todo }) {
-	return <div className='todo'>{todo.text}</div>
+function Todo({ todo, index, completeTodo }) {
+	return (
+		<div
+			className='todo'
+			style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+		>
+			{todo.text}
+			<div>
+				<button onClick={() => completeTodo(index)}>Complete</button>
+			</div>
+		</div>
+	)
 }
 
 export default Todo
